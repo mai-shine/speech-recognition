9346a4316bb9b4d3a3cf1a1901d625620a69915f
Helena Meissner <maimei@tzi.de>
2016-12-12 16:22:31 +0200

    HHEd fix

--- project/model_train/triphone.sh
+++ project/model_train/triphone.sh
@@ -12,24 +12,22 @@ PATH="$PATH:/home/maimei/Documents/STUDIES/9.lukukausi/08_Speech_Recognition/Pro
 # Cloning
 # use monophones0 (skip step 7, where monohpones1 is created)
 # run for acoustic model
-mkdir triphone/hmm6
-HHEd -B -H monophone/hmm5/macros -H monophone/hmm5/hmmdefs -M triphone/hmm6 mktri.hed monophone/monophones0
+#mkdir triphone/hmm6
+#HHEd -B -H monophone/hmm5/hmmdefs -M triphone/hmm6 mktri.hed monophone/monophones0
 
 # run for duration mode
-#HHEd -B -H monophone/hmm5/macros -H monophone/hmm5/hmmdefs.dur -M triphone/hmm6 mktri.hed monophone/monophones0
-
-
+#HHEd -B -H monophone/hmm5/hmmdefs.dur -M triphone/hmm6 mktri.hed monophone/monophones0
 
 
 
 # Re-estimation for 2 iters
 lab_path=/home/maimei/Documents/STUDIES/9.lukukausi/08_Speech_Recognition/Project/Code/project/files/labels/train/triphone
 
-#HERest -B -C config -L $lab_path -t 250.0 150.0 1000.0 -s stats \
-#-S train.scp -H monophone/hmm5/macros -H monophone/hmm5/hmmdefs -M triphone/hmm6 \
-#-N monophone/hmm5/hmmdefs.dur -u mvwtdmv -R triphone/hmm6 triphone/triphones1 triphone/triphones1
+HERest -B -C config -L $lab_path -t 250.0 150.0 1000.0 -s stats \
+-S train.scp -H monophone/hmm5/hmmdefs -M triphone/hmm6 \
+-N monophone/hmm5/hmmdefs.dur -u mvwtdmv -R triphone/hmm6 triphone/triphones1 triphone/triphones1
 
-#HERest -B -C config -L $lab_path -t 250.0 150.0 1000.0 -s stats \
-#-S train.scp -H triphone/hmm6/macros -H triphone/hmm6/hmmdefs -M triphone/hmm7 \
-#-N triphone/hmm6/hmmdefs.dur -u mvwtdmv -R triphone/hmm7 triphone/triphones1 triphone/triphones1
+HERest -B -C config -L $lab_path -t 250.0 150.0 1000.0 -s stats \
+-S train.scp -H triphone/hmm6/hmmdefs -M triphone/hmm7 \
+-N triphone/hmm6/hmmdefs.dur -u mvwtdmv -R triphone/hmm7 triphone/triphones1 triphone/triphones1
 
